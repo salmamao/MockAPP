eec11eeefd5c29ac9c1612b5e128dc57bc19bcd8
salma <salma.17m@gmail.com>
2016-09-06 20:47:06 +0100

    MKAPP-12: adding the ArticleRepository to article entity

--- src/AppBundle/Entity/Article.php
+++ src/AppBundle/Entity/Article.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ArticleRepository")
  * @ORM\Table(name="article")
  */
 class Article
