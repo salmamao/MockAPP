2c79d4a423fdd5b10d3d005da955e7eca5d80617
salma <salma.17m@gmail.com>
2016-09-08 22:16:37 +0100

    MKAPP-14: creating ListArticleService.php file

--- src/AppBundle/Service/ListArticleService.php
+++ src/AppBundle/Service/ListArticleService.php
@@ -9,7 +9,26 @@
 namespace AppBundle\Service;
 
 
+use AppBundle\Entity\Article;
+use AppBundle\Repository\ArticleRepository;
+
 class ListArticleService
 {
+    protected $articleRepository;
+
+
+    public function __construct(ArticleRepository $articleRepository)
+    {
+        $this->articleRepository = $articleRepository;
+    }
+
+    public function displayArticles(int $userId)
+    {
+        $articles = $this->articleRepository->findBy(
+            array('user_id' => $userId)
+        );
+
+        return $articles;
+    }
 
 }
